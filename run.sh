4920b399a819b8e738020a4344a7c41da0b65d6e
unknown <ro.esta07@gmal.com>
2022-05-17 10:15:29 +0545

    few changes to Path

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
-if [ $1 == 1 ]
+if [ $1 == "data" ]
 then
     python -m scr.print
     #python -m scr.get_images
-elif [ $1 == 2 ]
+elif [ $1 == "image" ]
 then
     python -m scr.get_data
 else
     echo "enter either pic or data"
 fi
-#python -m scr.attem
-#python -m scr.main
--- scr/settings.py
+++ scr/settings.py
@@ -3,19 +3,28 @@ from pathlib import Path
 ## all the path information##
 
 BASE_DIR=Path(__file__).parent.parent
-DESKTOP_DIR= Path.home()/ "Desktop" 
+DESKTOP_DIR= Path.home()/ "Desktop"
+
 ##credential path
-CLIENT_SECRET_FILE=BASE_DIR / 'creds' / 'client_secret.json'
+CREDS_DIR=BASE_DIR / 'creds'
+CLIENT_SECRET_FILE=CREDS_DIR / 'client_secret.json'
 PIC_DIR_PATH=DESKTOP_DIR / "HRIS" #where image will be saved
 TOKEN_DIR=BASE_DIR / "creds"
 TOKEN_PATH_SHEETS=TOKEN_DIR / "token_sheets.json"
 TOKEN_PATH_DRIVE=TOKEN_DIR / "token_drive_v3.pickle"
 INPUT_DIR=BASE_DIR / "input"
+
+if not CREDS_DIR.exists():
+    CREDS_DIR.mkdir(parents=True,exist_ok=True)
+
 if not PIC_DIR_PATH.exists():
     PIC_DIR_PATH.mkdir(parents=True,exist_ok=True)
+
 if not INPUT_DIR.exists():
     INPUT_DIR.mkdir(parents=True,exist_ok=True)
+
 INPUT_DATA_PATH= INPUT_DIR / 'storage_data.csv'
+
 ## drive api information
 
 API_NAME='drive'
