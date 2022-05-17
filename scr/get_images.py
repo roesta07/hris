11c74beecb3e9a7d218117b895e49d1556da1d54
unknown <ro.esta07@gmal.com>
2022-05-17 14:28:38 +0545

    creating done.csv enabled

--- scr/get_images.py
+++ scr/get_images.py
@@ -51,44 +51,50 @@ filt=df_storage['data-Name']=='' ## remove
 df_storage=df_storage.loc[~filt]
 df=pd.DataFrame()
 
-def get_datasets(EXPORT=True):
-    df_storage=get_sheets(
-        sheet_filename='Storage_',
-        work_sheet_name='Sheet1',
-        TITLE_ROW= 1,
-        TOKEN_PATH=TOKEN_PATH_SHEETS
-    )
-    
-    df=df.assign(
-        id=df_storage['data-meta-instanceID'],
-        email=df_storage['data-e-mail'].replace({'Unspecified':pd.NA}),
-        phone=df_storage['data-phone_1_-_Value'].apply(lambda x:'977'+ x if x!='null' else pd.NA),
-        name=df_storage['data-Name'],
-        ct=df_storage['data-address_1_-_city'].apply(str.lower).replace({'unspecified':pd.NA}).values,
-        country='NP',
-        dob=df_storage['data-Year_of_Birth'],
-        today=df_storage['data-today'],
-        photo_id=[get_pic_id(photo) for photo in df_storage['data-photo'].values],
-        fn=lambda d: d['name'].apply(get_fn),
-        ln=lambda d: d['name'].apply(get_ln),
-        age=lambda d: d['dob'].apply(get_age),
-        lisc=df_storage['data-Driving_License'].replace({
-            'Unspecified':'idk', 'no':'False', 'category_a__motorcycle_scooter':'A',
-            'category_a__motorcycle_scooter':'A', 'category_b__car_jeep_delivery':"B",
-            'category_k__scooter__moped':"K", '':'idk', 'yes':'True',
-            'category_a__motorcycle_scooter':"A",
-            'category_a__motorcycle_scooter, no':"A",
-            'category_b__car_jeep_delivery':"B",
-            'category_b__car_jeep_delivery':"B", "category_a__motorcycle_scooter":"A",
-            'no, category_a__motorcycle_scooter':"A",
-            'category_b__car_jeep_delivery, category_a__motorcycle_scooter':'AB',
-            'category_a__motorcycle_scooter, category_b__car_jeep_delivery':'AB',
-    }),
-    )
+
+df_storage=get_sheets(
+    sheet_filename='Storage_',
+    work_sheet_name='Sheet1',
+    TITLE_ROW= 1,
+    TOKEN_PATH=TOKEN_PATH_SHEETS
+)
+
+df=df.assign(
+    id=df_storage['data-meta-instanceID'],
+    email=df_storage['data-e-mail'].replace({'Unspecified':pd.NA}),
+    phone=df_storage['data-phone_1_-_Value'].apply(lambda x:'977'+ x if x!='null' else pd.NA),
+    name=df_storage['data-Name'],
+    ct=df_storage['data-address_1_-_city'].apply(str.lower).replace({'unspecified':pd.NA}).values,
+    country='NP',
+    dob=df_storage['data-Year_of_Birth'],
+    today=df_storage['data-today'],
+    photo_id=[get_pic_id(photo) for photo in df_storage['data-photo'].values],
+    fn=lambda d: d['name'].apply(get_fn),
+    ln=lambda d: d['name'].apply(get_ln),
+    age=lambda d: d['dob'].apply(get_age),
+    lisc=df_storage['data-Driving_License'].replace({
+        'Unspecified':'idk', 'no':'False', 'category_a__motorcycle_scooter':'A',
+        'category_a__motorcycle_scooter':'A', 'category_b__car_jeep_delivery':"B",
+        'category_k__scooter__moped':"K", '':'idk', 'yes':'True',
+        'category_a__motorcycle_scooter':"A",
+        'category_a__motorcycle_scooter, no':"A",
+        'category_b__car_jeep_delivery':"B",
+        'category_b__car_jeep_delivery':"B", "category_a__motorcycle_scooter":"A",
+        'no, category_a__motorcycle_scooter':"A",
+        'category_b__car_jeep_delivery, category_a__motorcycle_scooter':'AB',
+        'category_a__motorcycle_scooter, category_b__car_jeep_delivery':'AB',
+}),
+)
 
 empty_id_filter=df['photo_id'].isna()
-
 df_filtered=df.loc[~empty_id_filter]
+
+DONE_FILE_CSV_PATH=BASE_DIR / "scr" / "done.csv"
+
+if not DONE_FILE_CSV_PATH.exists():
+    df_done=pd.DataFrame(columns=df.columns)
+    df_done.to_csv(DONE_FILE_CSV_PATH,index=None)
+
 existing_pics=pd.read_csv("scr/done.csv").phone.values
 already_downloaded_filt= np.array([True if pic in existing_pics else False for pic in df_filtered['phone']])
 print(~already_downloaded_filt)
@@ -103,9 +109,9 @@ def assign_filename(df_filtered):
      
         lst.append(f'{name}_{phone}_{lis}.jpg')
     return np.array(lst)
+
 file_ids=df_filtered['photo_id'].values
 file_names=assign_filename(df_filtered)
-
 
 def download_media(service,file_ids,file_names,pic_store_path):
     for file_id,file_name in zip(file_ids,file_names,):
